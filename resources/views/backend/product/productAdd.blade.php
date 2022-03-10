1ca33b7244e73f3d90999aef5ac21f7a3b2711a0
MirajHoque <a.h.miraj2031@gmail.com>
2022-03-11 00:10:21 +0600

    updated the product add ui

--- resources/views/backend/product/productAdd.blade.php
+++ resources/views/backend/product/productAdd.blade.php
@@ -76,14 +76,176 @@
 
 
                         </div>  <!--end 1st row-->
+
+                        <div class="row"> <!--Start 2nd row-->
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <h5>Select Sub SubCategory</h5>
+                                    <div class="controls">
+                                    <select name="subsubcategory_id" id="subsubcategory_id" class="form-control">
+                                        <option value="" selected disabled>Select Brand</option>
+                                    </select>
+                                    <span class="text-danger" id="subsubcategory_id_error"></span>
+                                    </div>
+                                </div>
+                                 
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Name English<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_name-en" id="product_name-en" class="form-control">
+                                        <span class="text-danger" id="product_name-en_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Name Bengali<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_name_ban" id="product_name_ban" class="form-control">
+                                        <span class="text-danger" id="product_name_ban_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+                            
+
+
+
+                        </div>  <!--end 2nd row-->     
+                        
+                        <div class="row"> <!--Start 3rd row-->
+
+                            <div class="col-md-4">
+                                
+                                <div class="form-group">
+                                    <h5>Product Code<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_code" id="product_code" class="form-control">
+                                        <span class="text-danger" id="product_code_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Quantity<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_qty" id="product_qty" class="form-control">
+                                        <span class="text-danger" id="product_qty_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Tags English<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_tag_en" id="product_tag_en" value="Lorem,Ipsum,Amet" data-role="tagsinput" class="form-control">
+                                        <span class="text-danger" id="product_tag_en_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->                            
+
+                        </div>  <!--end 3rd row--> 
+
+
+
+                        <div class="row"> <!--Start 4th row-->
+
+                            <div class="col-md-4">
+                                
+                                <div class="form-group">
+                                    <h5>Product Tags Bengali<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_tag_ban" id="product_tag_ban" value="Lorem,Ipsum,Amet" data-role="tagsinput"  class="form-control">
+                                        <span class="text-danger" id="product_tag_ban_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Size English<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_size_en" id="product_size_en" value="Small,Medium,Large" data-role="tagsinput"  class="form-control">
+                                        <span class="text-danger" id="product_size_en_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Size Bengali<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_size_ban" id="product_size_ban" value="ছোট,মাঝারি,বড়" data-role="tagsinput" class="form-control">
+                                        <span class="text-danger" id="product_size_ban_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+                            
+
+
+
+                        </div>  <!--end 4th row--> 
+
+
+                        <div class="row"> <!--Start 5th row-->
+
+                            <div class="col-md-4">
+                                
+                                <div class="form-group">
+                                    <h5>Product Color English<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_color_en" id="product_color_en" value="Red,Green,Blue" data-role="tagsinput"  class="form-control">
+                                        <span class="text-danger" id="product_color_en_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Color Bengali<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="text" name="product_color_ban" id="product_color_ban" value="লাল,সবুজ,নীল" data-role="tagsinput"  class="form-control">
+                                        <span class="text-danger" id="product_color_ban_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+
+                            <div class="col-md-4"> 
+                                <div class="form-group">
+                                    <h5>Product Selling Price<span class="text-danger"></span></h5>
+                                    <div class="controls">
+                                        <input type="value" name="selling_price" id="selling_price" class="form-control">
+                                        <span class="text-danger" id="selling_price_error"></span>
+                                    </div>
+                                </div>
+
+                            </div>  <!--end column-->
+                            
+
+
+
+                        </div>  <!--end 5th row--> 
                         
                         
 
-                        <div class="form-group">
-                            <h5>Email Field <span class="text-danger">*</span></h5>
-                            <div class="controls">
-                                <input type="email" name="email" class="form-control" required data-validation-required-message="This field is required"> </div>
-                        </div>
+                        
                         <div class="form-group">
                             <h5>File Input Field <span class="text-danger">*</span></h5>
                             <div class="controls">
@@ -156,5 +318,8 @@
     <!-- /.content -->
 </div>
 
-
 @endsection
+
+@push('script')
+<script src="{{asset('assets/vendor_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js') }}"></script>
+@endpush
