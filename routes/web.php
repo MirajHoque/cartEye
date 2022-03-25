45e4778d47c66237ba0a15ee5773bd54604ee6bb
MirajHoque <a.h.miraj2031@gmail.com>
2022-03-25 18:26:26 +0600

    fixing admin login issue

--- routes/web.php
+++ routes/web.php
@@ -34,17 +34,22 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function(){
     Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
 });
 
-Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
-    return view('admin.index');
-})->name('dashboard');
+Route::middleware(['auth:admin'])->group(function(){
+    Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
+        return view('admin.index');
+    })->name('dashboard')->middleware('auth:admin');
+    
+     
+    Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminProfileController::class, 'adminProifle'])->name('admin.profile');
+    Route::get('/admin/profile/edit', [AdminProfileController::class, 'adminProifleEdit'])->name('admin.profile.edit');
+    Route::post('/admin/profile/store', [AdminProfileController::class, 'store'])->name('admin.profile.update');
+    Route::get('/admin/change/password', [AdminProfileController::class, 'changePassword'])->name('admin.change.password');
+    Route::post('/admin/update/password', [AdminProfileController::class, 'updatePassword']);
+    
 
- 
-Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
-Route::get('/admin/profile', [AdminProfileController::class, 'adminProifle'])->name('admin.profile');
-Route::get('/admin/profile/edit', [AdminProfileController::class, 'adminProifleEdit'])->name('admin.profile.edit');
-Route::post('/admin/profile/store', [AdminProfileController::class, 'store'])->name('admin.profile.update');
-Route::get('/admin/change/password', [AdminProfileController::class, 'changePassword'])->name('admin.change.password');
-Route::post('/admin/update/password', [AdminProfileController::class, 'updatePassword']);
+});
+
 
 /*************************************
             Brand Route
@@ -133,7 +138,7 @@ Route::prefix('slider')->group(function(){
             User Route
  *************************************/
 
-Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum,web', 'verified'])->get('web/dashboard', function () {
     $id = Auth::user()->id;
     //dd($id);
     $user = User::find($id);
