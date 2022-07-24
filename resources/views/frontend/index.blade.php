f59dff073c97dc839dad60a189c18d56d34e7fea
MirajHoque <a.h.miraj2031@gmail.com>
2022-07-25 00:05:23 +0600

    all product in category all section price update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -974,17 +974,39 @@
                               <a href="detail.html"><img  src="{{ asset($product->product_thumbnail) }}" alt=""></a> 
                             </div>
                             <!-- /.image -->
-                            
-                            <div class="tag new"><span>new</span></div>
+                            @php
+                              $amount =$product->selling_price - $product->discount_price;
+                              $discount = ($amount/$product->selling_price) * 100;
+                            @endphp
+
+                            <div>
+                              @if ($product->discount_price == null)
+                              <div class="tag new"><span>new</span></div>
+                              @else
+                              <div class="tag hot"><span>{{ round($discount) }}%</span></div>
+                              @endif
+                            </div>
                           </div>
                           <!-- /.product-image -->
                           
                           <div class="product-info text-left">
-                            <h3 class="name"><a href="detail.html">{{ $product->product_name_en }}</a></h3>
+                            <h3 class="name">
+                              @if (session()->get('language') == 'Bengali')
+                              <a href="detail.html">{{ $product->product_name_ban }}</a>
+                              @else
+                              <a href="detail.html">{{ $product->product_name_en }}</a>
+                              @endif
+                              
+                            </h3>
                             <div class="rating rateit-small"></div>
                             <div class="description"></div>
-                            <div class="product-price"> <span class="price">{{ $product->selling_price }}</span>
-                              <span class="price-before-discount">$ 800</span>
+                            <div class="product-price">
+                              @if ($product->discount_price == null)
+                              <span class="price">{{ $product->selling_price }}</span>
+                              @else
+                              <span class="price">{{ $product->discount_price }}</span>
+                              <span class="price-before-discount">{{ $product->selling_price }}</span>
+                              @endif
                             </div>
                             <!-- /.product-price --> 
                             
@@ -1046,7 +1068,14 @@
                           <!-- /.product-image -->
                           
                           <div class="product-info text-left">
-                            <h3 class="name"><a href="detail.html">{{ $product->product_name_en }}</a></h3>
+                            <h3 class="name">
+                              {{-- @if (session()->get('language') == 'Bengali')
+                              <a href="detail.html">{{ $product->product_name_ban  }}</a>
+                              @else
+                              <a href="detail.html">{{ $product->product_name_en }}</a>
+                              @endif --}}
+                             
+                            </h3>
                             <div class="rating rateit-small"></div>
                             <div class="description"></div>
                             <div class="product-price"> <span class="price">{{ $product->selling_price }}</span>
